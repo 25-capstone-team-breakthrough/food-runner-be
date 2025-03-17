656b8447a17c092ecc9dc4bde1a6cdcb188a1b06
FriedKimBap <rladudals4356@gmail.com>
2025-03-18 01:46:45 +0900

    imagemeallog

---
+++ src/main/java/com/Hansung/Capston/entity/ImageMealLog.java
@@ -0,0 +1,29 @@
+package com.Hansung.Capston.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Image_Meal_Log")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ImageMealLog {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // MySQL AUTO_INCREMENT 적용
+  @Column(name = "image_meal_log_id", nullable = false)
+  private Long imageMealLogId;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "meal_id", nullable = false)
+  private MealLog mealLog;  // MealLog와 연결
+
+  @Column(name = "meal_name", nullable = false)
+  private String mealName;
+
+  @Column(name = "meal_image", nullable = false)
+  private String mealImage;
+}
