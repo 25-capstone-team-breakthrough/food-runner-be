d2a9ad9d3654335699418a0e4b31c18af8094c98
FriedKimBap <rladudals4356@gmail.com>
2025-05-08 02:50:50 +0900

    food data 추가

--- src/main/java/com/Hansung/Capston/controller/DataController.java
+++ src/main/java/com/Hansung/Capston/controller/DataController.java
@@ -52,7 +52,7 @@ public class DataController {
   public ResponseEntity<String> uploadCsvFile(@RequestParam("file") MultipartFile file) {
     try {
       // 업로드된 CSV 파일을 서버에 저장하고 처리
-      dataService.processFoodCsvFile(file);ㅎ
+      dataService.processFoodCsvFile(file);
       return ResponseEntity.ok("음식 데이터가 성공적으로 업로드되었습니다.");
     } catch (IOException e) {
       return ResponseEntity.status(500).body("CSV 파일 처리 실패: " + e.getMessage());
