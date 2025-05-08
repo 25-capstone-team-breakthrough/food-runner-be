bd8738c482067a0ab9d5caeab2e236a4994f3e4b
hanle <21713223@gmail.com>
2025-05-08 14:40:16 +0900

    food

---
+++ src/main/java/com/Hansung/Capston/repository/Exercise/FoodDataSetRepository.java
@@ -0,0 +1,7 @@
+package com.Hansung.Capston.repository.Exercise;
+
+import com.Hansung.Capston.entity.Exercise.FoodDataSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodDataSetRepository extends JpaRepository<FoodDataSet, Long> {
+}
