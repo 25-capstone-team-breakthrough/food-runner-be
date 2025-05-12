43842084f927c9dc1a2c6cf78958c1c2ea3b1421
friedkimbap <rladudals4356@gmail.com>
2025-05-12 15:21:15 +0900

    nutrition_log save

--- src/main/java/com/Hansung/Capston/repository/Diet/Nutrition/NutritionLogRepository.java
+++ src/main/java/com/Hansung/Capston/repository/Diet/Nutrition/NutritionLogRepository.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NutritionLogRepository extends JpaRepository<NutritionLog, Long> {
 
@@ -12,5 +14,7 @@ public interface NutritionLogRepository extends JpaRepository<NutritionLog, Long
   List<NutritionLog> findByUserUserId(String user_userId);
 
   Optional<NutritionLog> findByUser_UserIdAndDate(String userId, LocalDate onlyDate);
-}
 
+  @Query("select * from nutition_log n where n.user_id = :userId and n.date = :onlydate")
+  NutritionLog findByUserIdAndDate(@Param("userId") String userId, @Param("onlydate") LocalDate onlyDate);
+}
