8568ef9e40fa969fde7d764cdbca12d6d09a196f
hanle <21713223@gmail.com>
2025-05-20 13:12:19 +0900

    인바디 수정

--- src/main/java/com/Hansung/Capston/dto/Inbody/InbodyDTO.java
+++ src/main/java/com/Hansung/Capston/dto/Inbody/InbodyDTO.java
@@ -2,12 +2,15 @@ package com.Hansung.Capston.dto.Inbody;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 
 public class InbodyDTO {
     private Integer pictureId;
     private Integer inbodyId;
     private String url;
+    private LocalDateTime createdAt;
 
     public InbodyDTO(Integer pictureId, Integer inbodyId, String url) {
         this.pictureId = pictureId;
